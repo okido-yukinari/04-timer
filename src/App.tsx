e1507234083199741acf485e4f80e64b0402682c
okido-yukinari <yukiti0525.revolution@au.com>
2024-09-06 22:59:47 +0900

    HTML要素実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Button,
+  IconButton,
+  Box,
+  Typography,
+  TextField,
+} from "@mui/material";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      sx={{
+        display: `flex`,
+        justifyContent: `center`,
+        alignItems: `center`,
+        height: `100vh`,
+      }}
+    >
+      <Card>
+        <CardContent sx={{ p: 4 }}>
+          <Typography sx={{ textAlign: `center` }}>タイマーアプリ</Typography>
+          <Box>
+            <TextField
+              id="outlined-number"
+              label="分"
+              type="number"
+              slotProps={{
+                inputLabel: {
+                  shrink: true,
+                },
+              }}
+            />
+            <TextField
+              id="outlined-number"
+              label="秒"
+              type="number"
+              slotProps={{
+                inputLabel: {
+                  shrink: true,
+                },
+              }}
+            />
+          </Box>
+
+          <Typography sx={{ textAlign: `center` }}>00:00</Typography>
+
+          <Box sx={{ display: `flex`, justifyContent: `center` }}>
+            <IconButton color="primary" size="large" aria-label="スタート">
+              <PlayArrowIcon fontSize="large" />
+            </IconButton>
+            <IconButton color="secondary" size="large" aria-label="一時停止">
+              <PauseIcon fontSize="large" />
+            </IconButton>
+            <IconButton color="error" size="large" aria-label="リセット">
+              <RestartAltIcon fontSize="large" />
+            </IconButton>
+          </Box>
+        </CardContent>
+      </Card>
+    </Container>
   );
 }
 
